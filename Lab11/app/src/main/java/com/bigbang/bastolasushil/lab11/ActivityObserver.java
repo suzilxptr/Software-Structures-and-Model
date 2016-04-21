f0e6d73ca310091b7ef20141c7dd7c1a62e46f4b
sushil.bastola <suzilxptr@gmail.com>
2016-04-21 10:31:58 +0300

    deleted

--- Lab11/app/src/main/java/com/bigbang/bastolasushil/lab11/ActivityObserver.java
+++
@@ -1,6 +0,0 @@
-package com.bigbang.bastolasushil.lab11;
-
-/**
- * Created by The BigBang on 5.4.2016.
- */
-
--- Lab15/app/src/main/java/com/bigbang/bastolasushil/lab15/Updater.java
+++
@@ -1,8 +0,0 @@
-package com.bigbang.bastolasushil.lab15;
-
-/**
- * Created by The BigBang on 5.4.2016.
- */
-public interface Updater {
-    public void update();
-}
